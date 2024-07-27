a627cba84fe53b819bbd65fa896aa02bc40722cc
lllggghhhaaa <lgha1423@outlook.com>
2024-07-27 16:48:55 -0300

    Fixed death note warn channel display

--- core/interactions/chunks/panel_guild_logger.js
+++ core/interactions/chunks/panel_guild_logger.js
@@ -97,7 +97,7 @@ module.exports = async ({ client, user, interaction, pagina_guia }) => {
                 },
                 {
                     name: `${client.defaultEmoji("channel")} **${client.tls.phrase(user, "mode.report.canal_de_avisos")}**`,
-                    value: guild.death_note.channel ? `${client.emoji("icon_id")} \`${guild.death_note_channel}\`\n( <#${guild.death_note_channel}> )` : `\`❌ ${client.tls.phrase(user, "mode.network.sem_canal")}\``,
+                    value: guild.death_note_channel ? `${client.emoji("icon_id")} \`${guild.death_note_channel}\`\n( <#${guild.death_note_channel}> )` : `\`❌ ${client.tls.phrase(user, "mode.network.sem_canal")}\``,
                     inline: true
                 }
             )
